5108afed8b29a335c64ea11d5fe60d37c4ef0151
EhODavi <davi.miau@gmail.com>
2021-04-28 20:14:11 -0300

    Commit

--- Trabalho III/B.cpp
+++ Trabalho III/B.cpp
@@ -1,49 +1,134 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-/*
-Sample Input
-3
-6 6 2
-1 1 6 6
-6 6 1 1
-7 7 4
-1 1 1 6
-6 1 6 6
-6 6 1 1
-4 3 5 1
-9 8 6
-2 2 4 4
-4 5 3 2
-3 4 2 2
-3 2 4 4
-4 5 2 2
-2 1 3 4
-Sample Output
-Yes
-No
-No
-*/
+#define f(i,k) for(int i = 0; i < k; i++)
 
+struct Sat2 {
+public:
+    Sat2(int numVariaveis) {
+        vars = numVariaveis;
+        implica = vector<vector<vector<int>>>(numVariaveis + 1, vector<vector<int>>(2));
+        valor = vector<int>(numVariaveis + 1, -1);     
+    }
+    
+    void addClausula(int c1, bool v1, int c2, bool v2) {
+        implica[c1][!v1].push_back(c2 * (v2 ? 1:-1));
+        implica[c2][!v2].push_back(c1 * (v1 ? 1:-1));
+    }
+
+    void addImplicacao(int c1, bool valor, int c2, bool valor2) {
+        addClausula(c1, !valor, c2, valor2);
+    }
+
+    bool satisfativel() {       
+        for(int i = 1; i <= vars; ++i) {
+           if(!verificaVariavel(i)) return false;
+        }
+        
+        return true;
+    }
+private:
+    vector<vector<vector<int>>> implica;
+    vector<int> valor;
+    int vars;
+    
+    bool funciona(int x, int val) {
+        if(valor[x] != -1) return valor[x] == val;
+
+        valor[x] = val;
+        
+        f(i, implica[x][val].size()) 
+            if(!funciona(abs(implica[x][val][i]), (implica[x][val][i] < 0 ? 0:1))) {
+                valor[x] = -1;
+                return false;
+            }                     
+        return true; 
+    }
+
+    bool verificaVariavel(int i) { 
+        f(k, vars + 1) valor[k] = -1;
+        if(funciona(i,0)) return true;
+        f(k, vars + 1) valor[k] = -1;
+        if(funciona(i,1)) return true;
+        
+        return false; 
+    }
+};
+
+void solve() {
+    int S, A, m, s1, a1, s2, a2;
+
+    cin >> S >> A >> m;
+
+    Sat2 cidade(S + A);
+
+    for(int i = 0; i < m; i++) {
+        cin >> s1 >> a1 >> s2 >> a2;
+
+        if(!(s1 == s2 && a1 == a2)) {
+            a1 += S;
+            a2 += S;
+
+            if(s1 == s2 && a1 < a2) { // verificar
+                cidade.addImplicacao(s1, false, s2, true);
+            } else if(s1 == s2 && a1 > a2) { // verificar
+                cidade.addImplicacao(s1, true, s2, false);
+            } else if(a1 == a2 && s1 < s2) { // verificar
+                cidade.addImplicacao(a1, true, a2, false);
+            } else if(a1 == a2 && s1 > s2) { // verificar
+                cidade.addImplicacao(a1, false, a2, true);
+            } else if(s1 < s2 && a1 < a2) {
+                cidade.addImplicacao(s1, false, a1, true);
+                cidade.addImplicacao(s1, false, s2, true);
+                cidade.addImplicacao(s2, false, s1, true);
+                cidade.addImplicacao(s2, false, a2, true);
+                cidade.addImplicacao(a1, false, s1, true);
+                cidade.addImplicacao(a1, false, a2, true);
+                cidade.addImplicacao(a2, false, a1, true);
+                cidade.addImplicacao(a2, false, s2, true);
+            } else if(s1 < s2 && a1 > a2) {
+                cidade.addImplicacao(s1, true, a1, true);
+                cidade.addImplicacao(s1, true, s2, false);
+                cidade.addImplicacao(s2, true, s1, false);
+                cidade.addImplicacao(s2, true, a2, true);
+                cidade.addImplicacao(a2, false, a1, true);
+                cidade.addImplicacao(a2, false, s2, false);
+                cidade.addImplicacao(a1, false, s1, false);
+                cidade.addImplicacao(a1, false, a2, true);
+            } else if(s1 > s2 && a1 < a2) {
+                cidade.addImplicacao(s1, false, a1, false);
+                cidade.addImplicacao(s1, false, s2, true);
+                cidade.addImplicacao(s2, false, s1, true);
+                cidade.addImplicacao(s2, false, a2, false);
+                cidade.addImplicacao(a1, true, s1, true);
+                cidade.addImplicacao(a1, true, a2, false);
+                cidade.addImplicacao(a2, true, a1, false);
+                cidade.addImplicacao(a2, true, s2, true);
+            } else if(s1 > s2 && a1 > a2) {
+                cidade.addImplicacao(s1, true, a1, false);
+                cidade.addImplicacao(s1, true, s2, false);
+                cidade.addImplicacao(s2, true, s1, false);
+                cidade.addImplicacao(s2, true, a2, false);
+                cidade.addImplicacao(a1, true, s1, false);
+                cidade.addImplicacao(a1, true, a2, false);
+                cidade.addImplicacao(a2, true, a1, false);
+                cidade.addImplicacao(a2, true, s2, false);
+            }
+        }
+    }
+
+    if(cidade.satisfativel()) cout << "Yes\n";
+    else cout << "No\n";
+}
+ 
 int main() {
-    // Problema de transito
-    // Tornar as ruas de mão única (novo plano de malha viária) enquanto mantêm uma curta distância entre alguns pares ordenados de locais
-    // As ruas formam uma grade ortogonal
-    // Avenidas (norte-sul) | Ruas (leste-oeste)
-    // Uma rota na cidade é definida por dois cruzamento de ruas, o local de partida e de meta
-    // Uma rota tem um caminho legal se for possível dirigir sem transgredir a lei e sair do inicio e chegar no destino
-    // Pode haver muitos caminhos possíveis para cada rota
-    // Um caminho legal é considerado simples se requer no máximo uma conversão, ou seja, podemos usar no máximo uma rua e uma avenida
-    // Um caminho simples terá preferência sobre um não simples por ser mais rápido
-    // Como a grade é de mão única, pode haver rotas para as quais não existe um caminho simples
-    // Na secretaria, há uma lista de percursos importantes que devem ter caminhos simples
-    // Escrever um programa que determina se será possível fixar as direções das ruas e avenidas de modo que cada rota da lista tenha pelo menos um caminho simples
-    // Na primeira linha da entrada há um único inteiro n informando quantos casos de testes
-    // Cada descrição de cidade começa com uma linha com três inteiros: S A m (S é o número de ruas, A é o número de avenidas, m é o número de rotas que devem ter pelo menos um caminho simples)
-    // As proximas m linhas definem estas rotas, uma em cada linha
-    // Cada definição de rota é da forma s1 a1 s2 a2 (em que o local de inicio é no cruzamento da rua s1 com a avenida a1 e o destino é o cruzamento da rua s2 com a avenida a2)
-    // Imprimir 'Yes' ou 'No'
+    int T;
 
-    return 0;
+    cin >> T;
+
+    for(int i = 0; i < T; i++) {
+        solve();
+    }
 }
